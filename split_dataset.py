c5f6bedd4b2363e796f23b973840a47bb5f56872
Dimitar Krastev <dimi1010100@gmail.com>
2019-09-13 11:35:42 +0300

    Renamed arts.test to args.dev. Adding args.output to specify output directory.

--- split_dataset.py
+++ split_dataset.py
@@ -7,7 +7,8 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--csv', help='CSV File', type=str)
 parser.add_argument('--train', help='Train indexes', type=str)
-parser.add_argument('--test', help='Test indexes', type=str)
+parser.add_argument('--dev', help='Dev indexes', type=str)
+parser.add_argument('--output', help='Output Dir', type=str)
 parser.add_argument('--tsv', help='Generates tsv instead', action='store_true')
 
 args = parser.parse_args()
@@ -22,21 +23,14 @@ def split_and_save():
     df['sentiment'] = df['rating'].apply(lambda rating: +1 if rating > 3 else -1)
 
     train_arr = pd.read_json(args.train)
-    test_arr = pd.read_json(args.test)
+    dev_arr = pd.read_json(args.dev)
 
     df_train: pd.DataFrame = df.iloc[train_arr[0]]
-    df_test: pd.DataFrame = df.iloc[test_arr[0]]
-
-    # with pd.option_context('display.max_rows', None, 'display.max_columns', 10):
-    #    print(df_train.iloc[:5])
-    #
-    # return None
-
-    path, _ = os.path.split(args.csv)
+    df_dev: pd.DataFrame = df.iloc[dev_arr[0]]
 
     if not args.tsv:
-        df_train.to_csv(path_or_buf=os.path.join(path, 'train.csv'))
-        df_test.to_csv(path_or_buf=os.path.join(path, 'test.csv'))
+        df_train.to_csv(path_or_buf=os.path.join(args.output, 'train.csv'))
+        df_dev.to_csv(path_or_buf=os.path.join(args.output, 'dev.csv'))
     else:
         df_train_tsv = pd.DataFrame({
             'id': range(len(df_train)),
@@ -45,11 +39,11 @@ def split_and_save():
             'text': df_train['review'].replace(r'\n',' ', regex=True)
         })
 
-        df_test_tsv = pd.DataFrame({
-            'id': range(len(df_test)),
-            'label': df_test['sentiment'],
-            'alpha': ['a'] * df_test.shape[0],
-            'text': df_test['review'].replace(r'\n', ' ', regex=True)
+        df_dev_tsv = pd.DataFrame({
+            'id': range(len(df_dev)),
+            'label': df_dev['sentiment'],
+            'alpha': ['a'] * df_dev.shape[0],
+            'text': df_dev['review'].replace(r'\n', ' ', regex=True)
         })
 
         with pd.option_context('display.max_rows', None, 'display.max_columns', 10):
@@ -57,18 +51,23 @@ def split_and_save():
 
         # return None
 
-        df_train_tsv.to_csv(path_or_buf=os.path.join(path, 'train.tsv'), sep='\t', index=False, header=False)
-        df_test_tsv.to_csv(path_or_buf=os.path.join(path, 'dev.tsv'), sep='\t', index=False, header=False)
+        df_train_tsv.to_csv(path_or_buf=os.path.join(args.output, 'train.tsv'), sep='\t', index=False, header=False)
+        df_dev_tsv.to_csv(path_or_buf=os.path.join(args.output, 'dev.tsv'), sep='\t', index=False, header=False)
 
 
 if __name__ == "__main__":
     if args.csv is None or not os.path.isfile(args.csv):
         raise FileNotFoundError
 
+    if args.output is None or not os.path.isdir(args.output):
+        print("--output is not specified or invalid. Using csv dir for output")
+
+        args.output, _ = os.path.split(args.csv)
+
     if args.train is None or not os.path.isfile(args.train):
         raise FileNotFoundError
 
-    if args.test is None or not os.path.isfile(args.test):
+    if args.dev is None or not os.path.isfile(args.dev):
         raise FileNotFoundError
 
     split_and_save()
